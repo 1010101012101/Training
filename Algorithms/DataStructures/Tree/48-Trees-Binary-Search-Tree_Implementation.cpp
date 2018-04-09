a789097b4ce29fbefa8986f2758f17dd796c8325
SREENIVASA  ARASANAGATTA <sreenivas.prestige@gmail.com>
2018-04-09 10:50:37 -0700

    Add files via upload

---
+++ Algorithms/DataStructures/Tree/48-Trees-Binary-Search-Tree_Implementation.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+
+struct BstNode
+{
+    int data;
+    BstNode * left;
+    BstNode* right;
+};
+
+BstNode* GetNode(int data)
+{
+    BstNode* newNode = new BstNode();
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+BstNode* Insert( BstNode* root, int data)
+{
+    if(root == NULL)  //empty node
+    {
+        root = GetNode(data);
+    }
+    else if( data <= root-> data)
+    {
+        root->left = Insert(root->left, data);
+    }
+    else
+    {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+
+bool Search(BstNode* root, int data )
+{
+    if( root == NULL) return false;
+    if(root->data == data) return true;
+    else if(data <= root->data) return Search(root->left, data);
+    else return Search(root->right, data);
+    
+}
+
+int main()
+{
+    BstNode* root = NULL;
+    
+     root = Insert(root,15);     root = Insert(root,10);     root = Insert(root,20);
+     root = Insert(root,25);     root = Insert(root,8);     root = Insert(root,12);
+     
+     int number;
+     cout << "Enter the Number to be searched: \n";
+     cin >> number;
+     
+    if(Search(root, number) == true)
+        cout << "found\n";
+    else
+        cout << "False\n";
+        
+     
+    
+    printf("Hello World");
+
+    return 0;
+}
